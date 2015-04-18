541cc514d77d4bc85605f0692e852739dba15e0b
Kris Kowal <kris.kowal@cixar.com>
2015-04-17 22:46:12 -0700

    Greatly simplify build script

--- build.sh
+++ build.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 set -e
 
-# function rdtree() {
-#     local DIR="$1"
-#     ls -- "$DIR" | while read FILE; do
-#         if [ -f "$DIR/$FILE" ]; then
-#             MODE=100644
-#             # if [ -x "$DIR/$FILE" ]; then
-#             #     MODE=100755
-#             # fi
-#             OBJECT=$(git hash-object -w --no-filters "$DIR/$FILE")
-#             if [ -n "$OBJECT" ]; then
-#                 echo "$MODE blob $OBJECT"$'\t'"$FILE"
-#             fi
-#         fi
-#         if [ -d "$DIR/$FILE" ]; then
-#             OBJECT=$(mktree "$DIR/$FILE")
-#             if [ -n "$OBJECT" ]; then
-#                 echo "040000 tree $OBJECT"$'\t'"$FILE"
-#             fi
-#         fi
-#     done
-# }
-# 
-# function mktree() {
-#     local DIR="$1"
-#     rdtree "$DIR" | git mktree
-# }
-
 HERE=$(cd -L $(dirname -- $0); pwd)
 export PATH="$HERE/node_modules/.bin":"$PATH"
 export GIT_DIR="$HERE/.git"
@@ -41,36 +14,21 @@ export GIT_COMMITTER_EMAIL="kris@cixar.com"
 export GIT_COMMITTER_DATE="$NOW"
 
 function gentree1() {
-    # NODE_MODULES=$(mktree $HERE/node_modules)
-    # echo "040000 tree $NODE_MODULES"$'\t'"node_modules"
-    # HTML_BUNDLE=$(git hash-object -w bundle.html)
-    # echo "100644 blob $HTML_BUNDLE"$'\t'"index.html"
     JS_BUNDLE=$(git hash-object -w <(mrs index.js))
     echo "100644 blob $JS_BUNDLE"$'\t'"bundle.js"
     CSS_BUNDLE=$(git hash-object -w <(lessc index.less))
     echo "100644 blob $CSS_BUNDLE"$'\t'"bundle.css"
+    HTML_BUNDLE=$(git hash-object -w bundle.html)
+    echo "100644 blob $HTML_BUNDLE"$'\t'"index.html"
 }
 
 OVERLAY=$(gentree1 | git mktree)
 git read-tree --empty
-git read-tree --prefix=/ refs/heads/master
-git add node_modules/tengwar/tengwar-{annatar,parmaite}.{css,eot,svg,woff} -f
 git read-tree --prefix=/ "$OVERLAY"
+git add node_modules/tengwar/tengwar-{annatar,parmaite}.{css,eot,svg,woff} -f
 TREE=$(git write-tree --missing-ok)
 PARENT=$(git rev-parse refs/heads/master)
 COMMIT=$(git commit-tree -p "$PARENT" "$TREE" < <(echo "Create bundles"))
-
-function gentree2() {
-    git ls-tree $PARENT | grep -v index.html
-    HTML_BUNDLE=$(git hash-object -w bundle.html)
-    echo "100644 blob $HTML_BUNDLE"$'\t'"index.html"
-}
-PARENT=$COMMIT
-OVERLAY=$(gentree2 | git mktree)
-git read-tree --empty
-git read-tree --prefix=/ "$OVERLAY"
-TREE=$(git write-tree --missing-ok)
-COMMIT=$(git commit-tree -p "$PARENT" "$TREE" < <(echo "Update index"))
 
 git update-ref refs/heads/gh-pages $COMMIT
 
